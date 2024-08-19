28fdca886ac3fb55279406b781587b1371fcfa25
Mohamed780050 <Mohamed39omer@gmail.com>
2024-08-19 11:30:26 +0300

    added the welcome page

--- src/components/FileContent.tsx
+++ src/components/FileContent.tsx
@@ -1,21 +1,28 @@
 import { RootState } from "@/Redux/store";
 import { useSelector } from "react-redux";
+import WelcomePage from "./WelcomePage";
 
 function FileContent() {
-  const { selectedFile } = useSelector((state: RootState) => state.files);
+  const { selectedFile,openFiles } = useSelector((state: RootState) => state.files);
   const Lines = selectedFile[0]?.content?.split("\n");
   let TheID = 0;
   return (
-    <ol className="p-2 overflow-scroll whitespace-nowrap contentScroll h-full bg-[#1f1f1f] text-white">
-      {Lines?.map((line, index) => (
-        <li key={++TheID} className="flex items-center justify-between">
-          <span className="px-2 py-0.5 min-w-10 text-center select-none">
-            {index + 1}
-          </span>
-          <pre className="pl-1 w-full text-left ">{line}</pre>
-        </li>
-      ))}
-    </ol>
+    <>
+      {openFiles.length ? (
+        <ol className="p-2 overflow-scroll whitespace-nowrap contentScroll h-full bg-[#1f1f1f] text-white">
+          {Lines?.map((line, index) => (
+            <li key={++TheID} className="flex items-center justify-between">
+              <span className="px-2 py-0.5 min-w-10 text-center select-none">
+                {index + 1}
+              </span>
+              <pre className="pl-1 w-full text-left ">{line}</pre>
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <WelcomePage />
+      )}
+    </>
   );
 }
 export default FileContent;
---
+++ src/components/WelcomePage.tsx
@@ -0,0 +1,8 @@
+function WelcomePage() {
+  return (
+    <div className="bg-[#1f1f1f] h-screen flex items-center justify-center">
+      <img className="w-64 h-64" src="/icons/vscode.svg" alt="VSCode icon" />
+    </div>
+  );
+}
+export default WelcomePage;
