17bb9a0c9aa9a9ead45c001a512097dae7abd054
Mohamed780050 <Mohamed39omer@gmail.com>
2024-08-21 13:00:11 +0300

    edited the open sequence and finished the projects

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,12 +1,12 @@
 import { RootState } from "@/Redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "./ui/button";
-import { removeFormOpenFiles } from "@/Redux/files";
+import { removeFormOpenFiles, setOpenSequence } from "@/Redux/files";
 import { setShow } from "@/Redux/contextmenu";
 import { useEffect } from "react";
 function ContextMenu() {
   const { position } = useSelector((state: RootState) => state.contextmenu);
-  const { openFiles, toRemove } = useSelector(
+  const { openFiles, toRemove, openSequence } = useSelector(
     (state: RootState) => state.files
   );
   const dispatch = useDispatch();
@@ -16,23 +16,28 @@ function ContextMenu() {
   useEffect(() => {
     window.addEventListener("click", showit);
     return () => window.removeEventListener("click", showit);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const OpenFiles = openFiles.filter((item) => item !== toRemove[0]);
+  const OpenSequence = openSequence.filter((item) => item !== toRemove[0]);
   return (
     <div
       onContextMenu={() => dispatch(setShow(false))}
-      className={`flex absolute bg-[#181818] p-2 rounded`}
+      className={`flex flex-col absolute bg-[#181818] p-2 rounded`}
       style={{ left: position.x, top: position.y }}
     >
       <Button
+        className="hover:bg-[#0078d4]"
         onClick={() => {
           dispatch(removeFormOpenFiles([...OpenFiles]));
+          dispatch(setOpenSequence(OpenSequence));
           dispatch(setShow(false));
         }}
       >
         Close
       </Button>
       <Button
+        className="hover:bg-[#0078d4]"
         onClick={() => {
           dispatch(removeFormOpenFiles([]));
           dispatch(setShow(false));
